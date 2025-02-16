2f5315c8dc694074b36d7de87a88af2c82783c96
satoko2024 <satokomtmt@gmail.com>
2025-02-16 19:11:30 +0900

    Lesson18課題提出１回目

--- src/main/java/com/techacademy/controller/UserController.java
+++ src/main/java/com/techacademy/controller/UserController.java
@@ -4,8 +4,8 @@ import java.util.Set;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult; // 追加
-import org.springframework.validation.annotation.Validated; // 追加
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,61 +25,48 @@ public class UserController {
         this.service = service;
     }
 
-    /** 一覧画面を表示 */
     @GetMapping("/list")
     public String getList(Model model) {
-        // 全件検索結果をModelに登録
         model.addAttribute("userlist", service.getUserList());
-        // user/list.htmlに画面遷移
         return "user/list";
     }
 
-    /** User登録画面を表示 */
     @GetMapping("/register")
     public String getRegister(@ModelAttribute User user) {
-        // User登録画面に遷移
         return "user/register";
     }
 
-    // ----- 変更ここから -----
-    /** User登録処理 */
     @PostMapping("/register")
     public String postRegister(@Validated User user, BindingResult res, Model model) {
-        if(res.hasErrors()) {
-            // エラーあり
+        if (res.hasErrors()) {
             return getRegister(user);
         }
-        // User登録
         service.saveUser(user);
-        // 一覧画面にリダイレクト
         return "redirect:/user/list";
     }
-    // ----- 変更ここまで -----
 
-    /** User更新画面を表示 */
     @GetMapping("/update/{id}/")
-    public String getUser(@PathVariable("id") Integer id, Model model) {
-        // Modelに登録
-        model.addAttribute("user", service.getUser(id));
-        // User更新画面に遷移
+    public String getUser(@PathVariable(name="id", required=false) Integer id, Model model) {
+        if (id != null) {
+            model.addAttribute("user", service.getUser(id));
+        } else {
+            model.addAttribute("user", model.getAttribute("user"));
+        }
         return "user/update";
     }
 
-    /** User更新処理 */
     @PostMapping("/update/{id}/")
-    public String postUser(User user) {
-        // User登録
+    public String postUser(@Validated @ModelAttribute User user, BindingResult res, Model model) {
+        if (res.hasErrors()) {
+            return getUser(null, model);
+        }
         service.saveUser(user);
-        // 一覧画面にリダイレクト
         return "redirect:/user/list";
     }
 
-    /** User削除処理 */
     @PostMapping(path="list", params="deleteRun")
     public String deleteRun(@RequestParam(name="idck") Set<Integer> idck, Model model) {
-        // Userを一括削除
         service.deleteUser(idck);
-        // 一覧画面にリダイレクト
         return "redirect:/user/list";
     }
 }
